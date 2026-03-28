a22d2ae6e7a6dc86804b5248c83465ea7e0b1b4b
Jelly Fish <karthikeyasomireddi19@gmail.com>
2026-03-28 22:11:42 +0530

    Added Generics.java to advanced

---
+++ advanced/Generics.java
@@ -0,0 +1,7 @@
+public class Generics {
+    public static void main(String[] args) {
+        // Advanced topic - Generics
+        // Coming soon - will learn this after basics!
+        System.out.println("Generics - Advanced Java Topic");
+    }
+}
