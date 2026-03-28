251c35e2888dee9e233c92cc56a431593a29964f
Jelly Fish <karthikeyasomireddi19@gmail.com>
2026-03-28 22:08:04 +0530

    Added HelloWorld.java to basics

---
+++ basics/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        // My first Java file in my portfolio!
+        System.out.println("Hello, World!");
+        System.out.println("I am Jelly Fish, and I am learning Java!");
+    }
+}
