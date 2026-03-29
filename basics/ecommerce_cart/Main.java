b97d3baccced1a946847e1370706c4ee98526d87
Jelly Fish <karthikeyasomireddi19@gmail.com>
2026-03-29 18:58:34 +0530

    Implemented working logic. Added Main.

---
+++ basics/ecommerce_cart/Main.java
@@ -0,0 +1,17 @@
+package ecommerce_cart;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Cart cart = new Cart();
+
+        Product p1 = new Product(1, "Laptop", 50000.0);
+        Product p2 = new Product(2, "Mouse", 500.0);
+
+        cart.addItem(p1, 1);
+        cart.addItem(p2, 2);
+
+        cart.showCart();
+        System.out.println("Total: " + cart.getTotal());
+    }
+}
