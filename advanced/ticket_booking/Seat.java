296f144bf66470dbe126cc13ec9f72e288146ce4
Jelly Fish <karthikeyasomireddi19@gmail.com>
2026-03-29 19:24:33 +0530

    Created ticket-booking system.

---
+++ advanced/ticket_booking/Seat.java
@@ -0,0 +1,33 @@
+package ticket_booking;
+
+public class Seat {
+
+    private int seatNumber;
+    private boolean isBooked = false;
+
+    public Seat(int seatNumber) {
+        this.seatNumber = seatNumber;
+    }
+
+
+    public int getSeatNumber() {
+        return seatNumber;
+    }
+
+    public boolean isBooked() {
+        return isBooked;
+    }
+
+    public boolean bookSeat() {
+
+        if (isBooked) {
+            System.out.println("Already Booked.");
+            return false;
+        } else {
+            System.out.println("Seat " + seatNumber + " Booked for you.");
+            isBooked = true;
+            return true;
+        }
+    }
+
+}
