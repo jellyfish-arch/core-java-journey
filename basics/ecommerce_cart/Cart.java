c0ba4b3eab66a356d7041209806da09211e929fb
Jelly Fish <karthikeyasomireddi19@gmail.com>
2026-03-30 19:13:12 +0530

    Added merge items.

--- basics/ecommerce_cart/Cart.java
+++ basics/ecommerce_cart/Cart.java
@@ -6,6 +6,14 @@ public class Cart {
     private List<CartItem> items = new ArrayList<>();
 
     public void addItem(Product product, int quantity) {
+
+        for (CartItem item : items) {
+            if (item.getProduct().getId() == product.getId()) {
+                item.setQuantity(item.getQuantity() + quantity);
+                return;
+            }
+        }
+
         items.add(new CartItem(product, quantity));
     }
 
