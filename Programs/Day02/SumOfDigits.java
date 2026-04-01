a4b5efdeacf6b86dc7aea3bf236b0820910907aa
Jelly Fish <karthikeyasomireddi19@gmail.com>
2026-04-01 23:15:10 +0530

    Added sum of the digits program.

---
+++ Programs/Day02/SumOfDigits.java
@@ -0,0 +1,15 @@
+package Day02;
+
+public class SumOfDigits {
+    public static void main(String[] args) {
+        int num = 12345;
+        int sum = 0;
+
+        while (num != 0) {
+            sum += num % 10;
+            num /= 10;
+        }
+
+        System.out.println("Sum: " + sum);
+    }
+}
