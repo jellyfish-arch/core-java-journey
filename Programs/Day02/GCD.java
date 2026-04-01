ab990781f5870b1dafbad1d2544dc0d4fb2cf9aa
Jelly Fish <karthikeyasomireddi19@gmail.com>
2026-04-01 23:13:21 +0530

    Added Greatest common divisor program.

---
+++ Programs/Day02/GCD.java
@@ -0,0 +1,16 @@
+package Day02;
+
+public class GCD {
+    public static void main(String[] args) {
+        int a = 48, b = 18;
+        int gcd = 1;
+
+        for (int i = 1; i <= a && i <= b; i++) {
+            if (a % i == 0 && b % i == 0) {
+                gcd = i;
+            }
+        }
+
+        System.out.println("GCD: " + gcd);
+    }
+}
