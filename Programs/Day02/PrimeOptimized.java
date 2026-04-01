a20c36f14f321bf2f242ef74f83e2ce4fa7b500d
Jelly Fish <karthikeyasomireddi19@gmail.com>
2026-04-01 23:12:15 +0530

    Added primeOptimized program.

---
+++ Programs/Day02/PrimeOptimized.java
@@ -0,0 +1,21 @@
+package Day02;
+
+public class PrimeOptimized {
+    public static void main(String[] args) {
+        int num = 29;
+        boolean isPrime = true;
+
+        if (num <= 1) {
+            isPrime = false;
+        } else {
+            for (int i = 2; i * i <= num; i++) {
+                if (num % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        System.out.println(num + " is " + (isPrime ? "Prime" : "Not Prime"));
+    }
+}
