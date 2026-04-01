6d63e760e25e86c0f6e263a33a4311e156b865ef
Jelly Fish <karthikeyasomireddi19@gmail.com>
2026-04-01 21:45:04 +0530

    Added Palindrome number check.

---
+++ Programs/PalindromeNumber.java
@@ -0,0 +1,19 @@
+public class PalindromeNumber {
+    public static void main(String[] args) {
+        int num = 121;
+        int original = num;
+        int reversed = 0;
+
+        while (num != 0) {
+            int digit = num % 10;
+            reversed = reversed * 10 + digit;
+            num /= 10;
+        }
+
+        if (original == reversed) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not Palindrome");
+        }
+    }
+}
