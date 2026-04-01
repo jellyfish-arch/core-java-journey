969c09950adec103f8613b6bc5f7cac8df81e904
Jelly Fish <karthikeyasomireddi19@gmail.com>
2026-04-01 23:16:16 +0530

    Added Strong number check.

---
+++ Programs/Day02/StrongNumber.java
@@ -0,0 +1,30 @@
+package Day02;
+
+public class StrongNumber {
+
+    public static int factorial(int n) {
+        int fact = 1;
+        for (int i = 1; i <= n; i++) {
+            fact *= i;
+        }
+        return fact;
+    }
+
+    public static void main(String[] args) {
+        int num = 145;
+        int original = num;
+        int sum = 0;
+
+        while (num != 0) {
+            int digit = num % 10;
+            sum += factorial(digit);
+            num /= 10;
+        }
+
+        if (sum == original) {
+            System.out.println("Strong Number");
+        } else {
+            System.out.println("Not Strong Number");
+        }
+    }
+}
