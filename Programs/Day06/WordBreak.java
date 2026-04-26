c8426bdaa7799742f0154c3b89a744a54972a24e
Jelly Fish <karthikeyasomireddi19@gmail.com>
2026-04-27 00:49:45 +0530

    Added word break program.

---
+++ Programs/Day06/WordBreak.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class WordBreak {
+    public static boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> wordSet = new HashSet<>(wordDict);
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true;
+
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && wordSet.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[s.length()];
+    }
+
+    public static void main(String[] args) {
+        String s = "leetcode";
+        List<String> wordDict = Arrays.asList("leet", "code");
+        System.out.println("WordBreak for 'leetcode': " + wordBreak(s, wordDict)); 
+
+        String s2 = "applepenapple";
+        List<String> dict2 = Arrays.asList("apple", "pen");
+        System.out.println("WordBreak for 'applepenapple': " + wordBreak(s2, dict2)); 
+
+        String s3 = "catsandog";
+        List<String> dict3 = Arrays.asList("cats", "dog", "sand", "and", "cat");
+        System.out.println("WordBreak for 'catsandog': " + wordBreak(s3, dict3)); 
+    }
+}
