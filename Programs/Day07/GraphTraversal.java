8635c1204e5ff5fcb06622f5d45c941d468db4ca
Jelly Fish <karthikeyasomireddi19@gmail.com>
2026-04-29 23:56:04 +0530

    feat: implement Day 07 Java programs including data structures, concurrency, networking, reflection, and algorithms.

---
+++ Programs/Day07/GraphTraversal.java
@@ -0,0 +1,56 @@
+package Day07;
+
+import java.util.*;
+
+public class GraphTraversal {
+    private int V;
+    private LinkedList<Integer> adj[];
+
+    @SuppressWarnings("unchecked")
+    public GraphTraversal(int v) {
+        V = v;
+        adj = new LinkedList[v];
+        for (int i = 0; i < v; ++i)
+            adj[i] = new LinkedList<>();
+    }
+
+    void addEdge(int v, int w) {
+        adj[v].add(w);
+    }
+
+    void BFS(int s) {
+        boolean visited[] = new boolean[V];
+        LinkedList<Integer> queue = new LinkedList<>();
+
+        visited[s] = true;
+        queue.add(s);
+
+        System.out.print("BFS: ");
+        while (queue.size() != 0) {
+            s = queue.poll();
+            System.out.print(s + " ");
+
+            Iterator<Integer> i = adj[s].listIterator();
+            while (i.hasNext()) {
+                int n = i.next();
+                if (!visited[n]) {
+                    visited[n] = true;
+                    queue.add(n);
+                }
+            }
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[]) {
+        GraphTraversal g = new GraphTraversal(4);
+        g.addEdge(0, 1);
+        g.addEdge(0, 2);
+        g.addEdge(1, 2);
+        g.addEdge(2, 0);
+        g.addEdge(2, 3);
+        g.addEdge(3, 3);
+
+        g.BFS(2);
+    }
+}
